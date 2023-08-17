eae6c193657fe3e989e9fa8b8984049cf44df044
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-17 09:48:17 +0330

    Update main

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int a = 50;
-        int b = 12;
+        int a = 12;
+        int b = 15;
         System.out.println(calculator.add(a, b));
         System.out.println(calculator.multiply(a, b));
         System.out.println(calculator.factorial(a));
