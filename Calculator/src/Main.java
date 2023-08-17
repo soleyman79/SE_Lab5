7196182a9d89db864897fc22ab961588866f0d34
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-17 09:50:27 +0330

    Get input by scanner

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int a = 12;
-        int b = 15;
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
         System.out.println(calculator.add(a, b));
         System.out.println(calculator.multiply(a, b));
         System.out.println(calculator.factorial(a));
