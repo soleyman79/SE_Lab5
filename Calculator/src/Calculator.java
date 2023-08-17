6ff9919b2aed391324bc67488faeb6cb650840e7
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-17 09:42:15 +0330

    Add factorial operation

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -9,4 +9,13 @@ public class Calculator {
     public int multiply(int a, int  b) {
         return a * b;
     }
+
+    public int factorial(int a) {
+        if (a == 0) {
+            return 1;
+        }
+        else {
+            return a * this.factorial(a - 1);
+        }
+    }
 }
