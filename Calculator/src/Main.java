bc9a0e3eaa3ce37af7fee15db725e11e8f4eb8e8
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-17 09:42:48 +0330

    Complete main flow

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator();
+        int a = 50;
+        int b = 12;
+        System.out.println(calculator.add(a, b));
+        System.out.println(calculator.multiply(a, b));
+        System.out.println(calculator.factorial(a));
     }
 }
