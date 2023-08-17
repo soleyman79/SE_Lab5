a974c6e88a5ddd3be2a135e55656a7edbdc81735
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-17 11:12:56 +0330

    Fix performance bug

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,24 +1,24 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        for (int i = 0; i <= num; i++) {
-            BigInteger answer = factorial(new BigInteger(String.valueOf(i)));
+        List<BigInteger> answers = new ArrayList<>();
+        answers.add(new BigInteger(String.valueOf("1")));
+        for (int i = 1; i <= num; i++) {
+            BigInteger answer = factorial(new BigInteger(String.valueOf(i)), answers.get(i - 1));
+            answers.add(answer);
         }
     }
 
-    public static BigInteger factorial(BigInteger n) {
-        BigInteger result = BigInteger.ONE;
-
-        while (n.compareTo(BigInteger.ZERO) > 0) {
-            result = result.multiply(n);
-            n = n.subtract(BigInteger.ONE);
-        }
+    public static BigInteger factorial(BigInteger n, BigInteger p) {
+        BigInteger result = p;
+        result = result.multiply(n);
 
         return result;
     }
 }
-
