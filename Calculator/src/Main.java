c353274bd8e9a238ff95aae440166018ba40ee7a
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2023-08-17 11:02:28 +0330

    Restructure

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,13 +1,24 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(calculator.add(a, b));
-        System.out.println(calculator.multiply(a, b));
-        System.out.println(calculator.factorial(a));
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            BigInteger answer = factorial(new BigInteger(String.valueOf(i)));
+        }
+    }
+
+    public static BigInteger factorial(BigInteger n) {
+        BigInteger result = BigInteger.ONE;
+
+        while (n.compareTo(BigInteger.ZERO) > 0) {
+            result = result.multiply(n);
+            n = n.subtract(BigInteger.ONE);
+        }
+
+        return result;
     }
 }
+
